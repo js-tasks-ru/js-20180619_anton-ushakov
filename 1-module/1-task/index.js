0c70fecee195c6e9e8c2ccdb66f7483ad72a7458
Anton Ushakov <anton_ushakov@inbox.ru>
2018-06-21 23:34:41 +0300

    задача 1-module-1-task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -5,12 +5,12 @@
  * @returns {number}
  */
 function pow (m, n) {
-    while ( n = 0){
-        var result =(m * m);
-        n = n - 1;
+    var result = 1;
+    for  ( i = 0;i<n;i++){
+        result =(result * m);
     }
     
         return result;
 }
 
-//console.log(pow(2,3));
+console.log(pow(2,3));
