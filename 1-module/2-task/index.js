51b02023b1b8f33eb5d2f32d72878fd12867c905
Anton Ushakov <anton_ushakov@inbox.ru>
2018-06-30 15:29:58 +0300

    1 module 2 task

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -1,3 +1,43 @@
+//'use strict';
+//
+///**
+// * Эту функцию трогать не нужно
+// */
+//function print (text) {
+//    alert(text);
+//}
+//
+//
+///**
+// * Эту функцию нужно поменять так,
+// * чтобы функция sayHello работала корректно
+// */
+//function isValid (name){
+//    
+//if (!name){
+//    return false;
+//} 
+//if (name.length < 4){
+//    return false;
+//}
+//if (name.indexOf(' ') !== -1 ){
+//    return false;
+//}
+//    
+//    return true;
+//}
+//
+//function sayHello () {
+//  
+//    let userName = prompt('Введите ваше имя');
+//    if (isValid(userName)) {
+//        print('Welcome back, ' + userName + '!');
+//    } else {
+//        print('Некорректное имя');
+//    }
+//}
+//sayHello();
+
 'use strict';
 
 /**
@@ -12,28 +52,25 @@ function print (text) {
  * Эту функцию нужно поменять так,
  * чтобы функция sayHello работала корректно
  */
-function isValid (name) {
-    if ((name === '') || (name.length < 5) ||(name.indexOf(' ') !== -1 )){
-        return false;
-    }
+function isValid (name){
+    if ((!name) || (name.length < 4) || (name.indexOf(' ') !== -1 )){
+        name = false;
+    }  
     else  {
-      return true;
-      
+       name = true;
     }
-    
+ 
+  return name;
+
 }
 
-
-
 function sayHello () {
+  
     let userName = prompt('Введите ваше имя');
-
     if (isValid(userName)) {
         print('Welcome back, ' + userName + '!');
     } else {
         print('Некорректное имя');
     }
 }
-
 sayHello();
-
